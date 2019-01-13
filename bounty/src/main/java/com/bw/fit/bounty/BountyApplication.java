e2bb00572dfe68a9bd2e89ad0dac6d73e87246fa
yanghui1103 <yanghui1103@foxmail.com>
2019-01-13 11:38:20 +0800

    bounty工程first

---
+++ bounty/src/main/java/com/bw/fit/bounty/BountyApplication.java
@@ -0,0 +1,14 @@
+package com.bw.fit.bounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BountyApplication.class, args);
+    }
+
+}
+
