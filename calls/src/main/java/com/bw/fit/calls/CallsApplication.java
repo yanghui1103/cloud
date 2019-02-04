d53a84b6acfff2fb4018d0388e60b170ea9457d3
yanghui1103 <yanghui1103@foxmail.com>
2019-02-04 17:52:37 +0800

    单笔服务编排案例

---
+++ calls/src/main/java/com/bw/fit/calls/CallsApplication.java
@@ -0,0 +1,14 @@
+package com.bw.fit.calls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallsApplication.class, args);
+    }
+
+}
+
