ec010cfb3e9873835a08ef61aa184b7aa6d5bb7b
yanghui1103 <yanghui1103@foxmail.com>
2019-02-24 20:20:53 +0800

    网关层增加统一异常过滤器

---
+++ api/src/main/java/com/bw/fit/pc/sys/filter/ErrorFilter.java
@@ -0,0 +1,34 @@
+package com.bw.fit.pc.sys.filter;
+
+import javax.servlet.http.HttpServletResponse;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ErrorFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        Throwable throwable = ctx.getThrowable();
+        ctx.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        ctx.set("error.exception", throwable.getCause());
+        return null;
+    }
+}
