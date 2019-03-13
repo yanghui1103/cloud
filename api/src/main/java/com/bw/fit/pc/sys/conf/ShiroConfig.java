b204da5dbec978cce841db2ee18636d465b423cd
yanghui1103 <yanghui1103@foxmail.com>
2019-03-13 21:10:49 +0800

    shiro授权完成

--- api/src/main/java/com/bw/fit/pc/sys/conf/ShiroConfig.java
+++ api/src/main/java/com/bw/fit/pc/sys/conf/ShiroConfig.java
@@ -1,40 +1,23 @@
 package com.bw.fit.pc.sys.conf;
 
-import com.bw.fit.pc.sys.controller.LoginController;
-import com.netflix.discovery.converters.Auto;
-import org.apache.shiro.authc.credential.CredentialsMatcher;
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.cache.ehcache.EhCacheManager;
-import org.apache.shiro.session.mgt.SessionManager;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * @Description
- * @Author yangh
- * @Date 2018-12-8 17:43
- * @Param ${PARAM}
- * @Return ${RETURN}
- * @VERSION
+ * @Description: Shiro的配置类
+ * @author: Gao Hang Hang
+ * @date 2019/01/22 11:05
  */
 @Configuration
 public class ShiroConfig {
-    private  static Logger logger = LoggerFactory.getLogger(ShiroConfig.class);
-
     /**
      * 密码校验规则HashedCredentialsMatcher
      * 这个类是为了对密码进行编码的 ,
@@ -51,13 +34,11 @@ public class ShiroConfig {
     }
 
     /**
-     * ShiroFilterFactoryBean 处理拦截资源文件问题。
-     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，因为在初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
-     * Filter Chain定义说明 1、一个URL可以配置多个Filter，使用逗号分隔 2、当设置多个过滤器时，全部验证通过，才视为通过
-     * 3、部分过滤器可指定参数，如perms，roles
+     * 创建ShiroFilterFactoryBean
+     * shiro过滤bean
      */
     @Bean
-    public ShiroFilterFactoryBean shirFilter(DefaultWebSecurityManager  securityManager) {
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         // 必须设置 SecurityManager
         shiroFilterFactoryBean.setSecurityManager(securityManager);
@@ -86,51 +67,31 @@ public class ShiroConfig {
     }
 
     /**
-     * 身份认证realm; (这个需要自己写，账号密码校验；权限等)
-     * @return
+     * 创建DefaultWebSecurityManager
      */
-    @Bean
-    public ShiroRealm myShiroRealm() {
-        ShiroRealm myShiroRealm = new ShiroRealm();
-        //myShiroRealm.setCredentialsMatcher(hashedCredentialsMatcher());
-        return myShiroRealm;
-    }
-
-
-    @Bean("sessionManager")
-    public SessionManager sessionManager(){
-        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        sessionManager.setSessionValidationSchedulerEnabled(true);
-        sessionManager.setSessionIdCookieEnabled(true);
-        return sessionManager;
-    }
-
-    @Bean("securityManager")
-    public DefaultWebSecurityManager securityManager(ShiroRealm shiroRealm, SessionManager sessionManager) {
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(shiroRealm);
-        securityManager.setSessionManager(sessionManager);
-
+        // 关联realm
+        securityManager.setRealm(userRealm);
         return securityManager;
     }
 
-    @Bean("lifecycleBeanPostProcessor")
-    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-        return new LifecycleBeanPostProcessor();
+    /**
+     * 创建Realm
+     */
+    @Bean(name = "userRealm") // bean的作用是让方法返回的对象放入spring环境
+    public UserRealm getRealm() {
+        UserRealm userRealm = new UserRealm();
+        //userRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return userRealm;
     }
 
+    /**
+     * 配置ShiroDialect，用于thymeleaf和shiro标签配合使用
+     */
     @Bean
-    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
-        proxyCreator.setProxyTargetClass(true);
-        return proxyCreator;
+    public ShiroDialect getShiroDialect() {
+        return new ShiroDialect();
     }
-
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
-        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
-        advisor.setSecurityManager(securityManager);
-        return advisor;
-    }
-
 }
---
+++ api/src/main/java/com/bw/fit/pc/sys/conf/UserRealm.java
@@ -0,0 +1,98 @@
+package com.bw.fit.pc.sys.conf;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.bw.fit.pc.sys.util.PropertiesUtil;
+import com.bw.fit.pc.sys.util.RestTemplateUtil;
+import com.google.common.base.Strings;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2019/01/22 11:07
+ */
+public class UserRealm extends AuthorizingRealm {
+    @Autowired
+    RestTemplate restTemplate;
+    @Resource
+    RestTemplateUtil restTemplateUtil;
+
+    /**
+     * 执行授权逻辑
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("执行授权逻辑");
+        JSONObject authsJSON =  restTemplate.getForObject("http://sys-proj/account/account/"+principalCollection.getPrimaryPrincipal(), JSONObject.class);
+
+        //SimpleAuthorizationInfo simpleAuthorInfo = new SimpleAuthorizationInfo();
+        //simpleAuthorInfo.addStringPermission("test");//给当前用户授权url为hello的权限码
+        //System.out.println("经试验：并不是每次调用接口就会执行，而是调用需要操作码（permission）的接口就会执行");
+        Set<String> auths = new HashSet<>();
+        if(authsJSON.getString("res").equals("2")){
+            Arrays.stream(authsJSON.getString("authCodes").split(",")).forEach(x->{
+                auths.add(x);
+            });
+        }
+
+        //创建SimpleAuthorizationInfo,并设置其roles属性
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(auths);
+        //返回SimpleAuthorizationInfo
+        return info;
+    }
+
+    /**
+     * 执行认证逻辑
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("执行认证逻辑");
+
+
+        //获取基于用户名和密码的令牌
+        //实际上这个authcToken是从LoginController里面currentUser.login(token)传过来的
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        if(token == null){
+            throw new AuthenticationException("认证失败，请重新登录");
+        }
+        String account = token.getUsername();
+        String password = String.valueOf(token.getPassword());
+        if(Strings.isNullOrEmpty(account)){
+            throw new AuthenticationException("账号不存在");
+        }
+        /****
+         * 根据账号获取账户详情
+         */
+        JSONObject accountJSON =  restTemplate.getForObject("http://sys-proj/account/account/"+account, JSONObject.class);
+        if("1".equals(accountJSON.get("res"))){throw new AuthenticationException(accountJSON.get("msg").toString());}
+        ByteSource salt = ByteSource.Util.bytes( PropertiesUtil.getValueByKey("user.pw.slogmm").toString() + account );
+//        AuthenticationInfo authcInfo=new SimpleAuthenticationInfo(account, PropertiesUtil.getValueByKey("sys.default.password").toString(), salt, getName());
+//
+//        //清之前的授权信息
+//        super.clearCachedAuthorizationInfo(authcInfo.getPrincipals());
+        //5,最后返回的用户信息，
+        Object principal = accountJSON.get("logName") ;
+        Object credentials = password ;
+        //6 盐值
+        SimpleAuthenticationInfo info = null ;
+        String name = getName();
+        info = new SimpleAuthenticationInfo(principal,credentials,salt,getName());
+        return info ;
+    }
+}
